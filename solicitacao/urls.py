96dc55744a15bad3ce72c00704fe0bcef13f6649
cledersonbc <cledersonbc@live.com>
2018-01-08 03:47:31 +0000

    Atualizacao das URLs

--- solicitacao/urls.py
+++ solicitacao/urls.py
@@ -4,42 +4,42 @@ from . import views
 app_name = 'solicitacao'
 
 urlpatterns = [
-    # /solicitacao
+    # /
     url(r'^$', views.index, name='index'),
 
-    # /solicitacao/login
+    # /login
     url(r'^login/$', views.login_usuario, name='login_usuario'),
 
-    # /solicitacao/logout
+    # /logout
     url(r'^logout/$', views.logout_usuario, name='logout_usuario'),
 
-    # /solicitacao/signup
+    # /signup
     url(r'^signup/$', views.signup, name='signup'),
 
-    # /solicitacao/perfil
+    # /perfil
     url(r'^perfil/$', views.perfil, name='perfil'),
 
-    # solicitacao/cadastro
+    # /cadastro
     url(r'^cadastro/$', views.cadastro, name='cadastro'),
 
-    # solicitacao/lista-solicitacoes
+    # /lista-solicitacoes
     url(r'^lista-solicitacoes/$', views.lista_solicitacoes, name='lista_solicitacoes'),
 
-    # solicitacao/lista-solicitacoes/del-solicitacao
+    # /lista-solicitacoes/del-solicitacao
     url(r'^lista-solicitacoes/del-solicitacao/(?P<solicitacao_id>[0-9]+)/$', views.del_solicitacao, name='del_solicitacao'),
 
-    # solicitacao/avaliar
-    url(r'^avaliar/(?P<solicitacao_id>[0-9]+)/$', views.avaliar, name='avaliar'),
+    # /solicitacao/avaliar
+    url(r'^solicitacao/avaliar/(?P<solicitacao_id>[0-9]+)/$', views.avaliar, name='avaliar'),
 
-    # solicitacao/lista-avaliacoes
+    # /lista-avaliacoes
     url(r'^lista-avaliacoes/$', views.lista_avaliacoes, name='lista_avaliacoes'),
 
-    # solicitacao/lista-solicitacoes/del-avaliacao
+    # /lista-solicitacoes/del-avaliacao
     url(r'^lista-avaliacoes/del-avaliacao/(?P<avaliacao_id>[0-9]+)/$', views.del_avaliacao, name='del_avaliacao'),
 
-    # solicitacao/avaliacao/visualizar
+    # /avaliacao/visualizar
     url(r'^avaliacao/visualizar/(?P<avaliacao_id>[0-9]+)/$', views.avaliacao_visualizar, name='avaliacao_visualizar'),
 
-    # solicitacao/visualizar
-    url(r'^visualizar/(?P<solicitacao_id>[0-9]+)/$', views.visualizar, name='visualizar'),
+    # /solicitacao/visualizar
+    url(r'^solicitacao/visualizar/(?P<solicitacao_id>[0-9]+)/$', views.visualizar, name='visualizar'),
 ]
